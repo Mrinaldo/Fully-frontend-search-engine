a0c0e296a9e3c803b80c0da7fb176ce716d1146f
Mrinaldo <mrinal.dharmik@gmail.com>
2019-05-28 15:02:49 +0530

    version 1.0 complete with a fully functioning main.js

--- main.js
+++ main.js
@@ -3,16 +3,6 @@ var search_string;
 var database = [];
 
 
-
-// constructor for page object which stores data of .html pages
-function page(name, url, key, text){
-	this.name = name;
-	this.url = url;
-	this.keywords = key;
-	this.text = text;
-}
-
-
 /* start of creation of database object */
 
 // creates a database object for all the webpages in a website which will help in searching
@@ -32,12 +22,20 @@ function makeSearchDatabase(url){
 
 // parses data from .html file and puts it into the database object
 function getDataFromFile(file){
+
+	// constructor for page object which stores data of .html/.php pages
+	function page(name, url, key, text){
+		this.name = name;
+		this.url = url;
+		this.keywords = key;
+		this.text = text;
+	}
+
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
 			var name;
 			var keywords;
-			var text = "";
 			var string = this.responseText;
 			var pos1 = string.indexOf("<meta");
 			var pos2;
@@ -45,16 +43,17 @@ function getDataFromFile(file){
 				pos2 = string.indexOf(">", pos1+5);
 				var str = string.slice(pos1+1, pos2);
 				pos1 = string.indexOf("<meta",pos2);
-				var content = str.split(/content[=\s\"]*/)[1].split(/\"/)[0];
-				if(str.indexOf("keywords") != -1){
-					keywords = content;
+				if(str.search(/name[=\s\"]*keywords/) != -1){
+					keywords = str.split(/content[=\s\"]*/)[1].split(/\"/)[0];
 				}
-				if(str.indexOf("description") != -1){
-					name = content;
+				if(str.search(/name[=\s"]*description/) != -1){
+					name = str.split(/content[=\s\"]*/)[1].split(/\"/)[0];
 				}
 			}
-			var data = string.split("<body>")[1].split(/[\s\n\t]*<[A-Za-z\s!\/\"\",=\.]*>[\s\n\t]*/);
-			for(i = 0; i<data.length; i++){
+
+			var data = string.split("<body>")[1].split(/[\s\n]*<[^>]*>[\s\n]*/);
+			var text = "";
+			for(i = 0; i < data.length; i++){
 				if(data[i]!=""){
 					text = text + data[i] + " ";
 				}
@@ -67,5 +66,56 @@ function getDataFromFile(file){
 }
 
 /* end of database object creation */
+
+
+
+/* start of search string response generation */
+
+function getSearchResponse(searchString){
+
+	var searchResponse = [];
+
+	// constructor for search results which stores name, url and search points
+	function searchResult(name, url, points){
+		this.name = name;
+		this.url = url;
+		this.points = points;
+	}
+
+	for( i = 0; i < database.length; i++){
+		var points = 0;
+		if(database[i].name.search(new RegExp(searchString, "i"))!=-1){
+			points += 20;
+		}
+		if(database[i].keywords.search(new RegExp(searchString, "i"))!=-1){
+			points += 10;
+		}
+		if(database[i].text.search(new RegExp(searchString, "i"))!=-1){
+			points += 5;
+		}
+		if(points!=0){
+			// only those results which have a search match are pushed into response
+			searchResponse.push(new searchResult(database[i].name, database[i].url, points));
+		}
+	}
+
+	// compare function for sorting array of objects
+	function compareResponse(a,b){
+		if(a.points > b.points)
+			return -1;
+		else if(a.points < b.points)
+			return 1;
+		else
+			return 0;
+	}
+
+	//sorting search response in descending order of points
+	searchResponse.sort(compareResponse);
+	delete searchResponse.filter(function(value){delete value.points;});
+	return searchResponse;
+}
+
+/* end of search string response generation. */
+
 
 
--- test/js/script.js
+++ test/js/script.js
@@ -1,1 +1,25 @@
 makeSearchDatabase("searchEngineUrls.txt");
+
+var searchEntry = document.getElementById("searchEntry");
+var searchOutput = document.getElementById("searchOutput");
+
+
+searchEntry.addEventListener("keydown",(function(event) {
+	/* Act on the event */
+	while (searchOutput.hasChildNodes()) {   
+		searchOutput.removeChild(searchOutput.firstChild);
+	}
+	var ul = document.createElement("ul");
+	var result = getSearchResponse(searchEntry.value);
+	for( i = 0; i < result.length; i++){
+		var li = document.createElement("li");
+		var a = document.createElement("a");
+		a.setAttribute("href", result[i].url);
+		var node = document.createTextNode(result[i].name);
+		a.appendChild(node);
+		li.appendChild(a);
+		ul.appendChild(li);
+		searchOutput.appendChild(ul);
+	}
+}));
+
